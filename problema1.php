7b30879b8bc777bff4027628256f8840ae18ee24
Pettinari <mauroliceo18@gmail.com>
2024-05-31 09:05:13 -0300

    Se creó la funcion agregarUsuario

---
+++ problema1.php
@@ -0,0 +1,20 @@
+<?php
+
+//declare(strict_types=1);
+
+function agregarUsuario($arrayAsociativo, $nombre, $edad, $email) {
+    $arrayNuevo = []//agregarle los parametros de la funcion
+    //agregarle los valores de array nuevo a array asociativo en el nuevo arrayAgregado
+
+    //return $arrayAgregado;
+}
+
+$personas = [
+    'nombres' => array("Juan","Mauro","Lionel"),
+    'edades' => array(18,17,38),
+    'emails' => array("juan@gmail.com","mauro@gmail.com","lionel@hotmail.com")
+];
+
+agregarUsuario($personas, "Alberto", 57, "alberto@hotmail.com")
+
+?>
